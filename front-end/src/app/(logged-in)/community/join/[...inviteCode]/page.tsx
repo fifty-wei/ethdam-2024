3e60f57320379972ab52276d4bd59654c9b8e114
akugone <dev@akumaw.com>
2024-04-14 09:03:36 +0200

    Fix ESLint ignoreDuringBuilds configuration

--- front-end/src/app/(logged-in)/community/join/[...inviteCode]/page.tsx
+++ front-end/src/app/(logged-in)/community/join/[...inviteCode]/page.tsx
@@ -1,5 +1,4 @@
-'use client';
-
+"use client";
 
 import { useEffect, useState } from "react";
 
@@ -12,110 +11,96 @@ import { Check } from "lucide-react";
 import Link from "next/link";
 
 export default function InviteCode() {
+  // Get the user address
+  const { address } = useAccount();
 
-    // Get the user address
-    const { address } = useAccount();
+  const { inviteCode } = useParams<{ inviteCode: string }>();
 
-    const { inviteCode } = useParams<{ inviteCode: string }>();
+  const apiSdk = new ApiSdk();
+  const apiKey = process.env.NEXT_PUBLIC_BANDADA_ADMIN_API_KEY!;
 
-    const apiSdk = new ApiSdk()
-    const apiKey = process.env.NEXT_PUBLIC_BANDADA_ADMIN_API_KEY!;
+  const [alreadyRedeem, setAlreadyRedeem] = useState(false);
+  const [isAccepted, setIsAccepted] = useState(false);
 
-    const [alreadyRedeem, setAlreadyRedeem] = useState(false);
-    const [isAccepted, setIsAccepted] = useState(false);
-
-
-    useEffect(() => {
-        if (!address) { return };
-        if (!inviteCode) { return };
-        if (alreadyRedeem) { return };
-        useInvitationCode();
-    }, [inviteCode]);
-
-    const useInvitationCode = async () => {
-        // Get the invite information
-        const invite = await apiSdk.getInvite(inviteCode, apiKey)
-        console.log(invite);
-
-        // Check if the invite code has already been redeemed
-        if (invite["isRedeemed"] === true) {
-            setAlreadyRedeem(true);
-            return;
-        }
-
-        // Use the invite code
-        const invitedResp = await apiSdk.addMemberByInviteCode(
-            invite["group"]["id"],
-            address,
-            inviteCode.toString()
-        );
-
-        console.log(invitedResp);
-
-        setIsAccepted(true);
+  useEffect(() => {
+    if (!address) {
+      return;
     }
+    if (!inviteCode) {
+      return;
+    }
+    if (alreadyRedeem) {
+      return;
+    }
+    const getInvitationCode = async () => {
+      // Get the invite information
+      const invite = await apiSdk.getInvite(inviteCode, apiKey);
+      console.log(invite);
 
-    return (
-        (isAccepted ? (
-            <>
+      // Check if the invite code has already been redeemed
+      if (invite["isRedeemed"] === true) {
+        setAlreadyRedeem(true);
+        return;
+      }
 
-                <div className="bg-background relative min-h-screen isolate overflow-hidden">
+      // Use the invite code
+      const invitedResp = await apiSdk.addMemberByInviteCode(
+        invite["group"]["id"],
+        address,
+        inviteCode.toString()
+      );
 
-                    <HeaderLoggedIn>
-                        <span className="font-semibold">Join the community</span>
-                        <Button size="icon" className="absolute right-2">
-                            <Check className="h-5 w-5" />
-                            <span className="sr-only">Join the community</span>
-                        </Button>
-                    </HeaderLoggedIn>
+      console.log(invitedResp);
 
-                    <div
-                        className="mx-auto flex items-center justify-center max-w-7xl px-6 pb-24 pt-10 sm:pb-40 lg:flex lg:px-8 lg:pt-40">
-                        <div
-                            className="mx-auto flex flex-col gap-8 items-center text-center max-w-6xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8">
-                                <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-                                    Congratulations, join the next chapter
-                                </h1>
+      setIsAccepted(true);
+    };
+    getInvitationCode();
+  }, [inviteCode, alreadyRedeem, address]);
 
-                                {/* TODO :: Redirect to a chapter */}
-                                <Button variant="outline">
-                                    <Link href={`/`}>
-                                        Next chapter
-                                    </Link>
-                                </Button>
-                        </div>
-                    </div>
+  return isAccepted ? (
+    <>
+      <div className="bg-background relative min-h-screen isolate overflow-hidden">
+        <HeaderLoggedIn>
+          <span className="font-semibold">Join the community</span>
+          <Button size="icon" className="absolute right-2">
+            <Check className="h-5 w-5" />
+            <span className="sr-only">Join the community</span>
+          </Button>
+        </HeaderLoggedIn>
 
+        <div className="mx-auto flex items-center justify-center max-w-7xl px-6 pb-24 pt-10 sm:pb-40 lg:flex lg:px-8 lg:pt-40">
+          <div className="mx-auto flex flex-col gap-8 items-center text-center max-w-6xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8">
+            <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+              Congratulations, join the next chapter
+            </h1>
 
-                </div>
+            {/* TODO :: Redirect to a chapter */}
+            <Button variant="outline">
+              <Link href={`/`}>Next chapter</Link>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
+  ) : (
+    <>
+      <div className="bg-background relative min-h-screen isolate overflow-hidden">
+        <HeaderLoggedIn>
+          <span className="font-semibold">Join the community</span>
+          <Button size="icon" className="absolute right-2">
+            <Check className="h-5 w-5" />
+            <span className="sr-only">Join the community</span>
+          </Button>
+        </HeaderLoggedIn>
 
-            </>
-        ) : (
-            <>
-                <div className="bg-background relative min-h-screen isolate overflow-hidden">
-
-                    <HeaderLoggedIn>
-                        <span className="font-semibold">Join the community</span>
-                        <Button size="icon" className="absolute right-2">
-                            <Check className="h-5 w-5" />
-                            <span className="sr-only">Join the community</span>
-                        </Button>
-                    </HeaderLoggedIn>
-
-                    <div
-                        className="mx-auto flex items-center justify-center max-w-7xl px-6 pb-24 pt-10 sm:pb-40 lg:flex lg:px-8 lg:pt-40">
-                        <div
-                            className="mx-auto flex flex-col gap-8 items-center text-center max-w-6xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8">
-                                <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-                                    The invite code is already used...
-                                </h1>
-                        </div>
-                    </div>
-
-                    
-
-                </div>
-            </>
-        ))
-    )
+        <div className="mx-auto flex items-center justify-center max-w-7xl px-6 pb-24 pt-10 sm:pb-40 lg:flex lg:px-8 lg:pt-40">
+          <div className="mx-auto flex flex-col gap-8 items-center text-center max-w-6xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8">
+            <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+              The invite code is already used...
+            </h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- front-end/src/components/form-give-feedback.tsx
+++ front-end/src/components/form-give-feedback.tsx
@@ -1,108 +1,104 @@
-import {useEffect} from 'react'
-import {Textarea} from "@/components/ui/textarea";
-import {Label} from "@/components/ui/label";
-import {Button} from "@/components/ui/button";
-import {Account} from "@/components/account";
-import {CircleUser, RefreshCw} from "lucide-react";
-import {StarRating} from "@/components/star-rating";
-import {useContractWrite, useWaitForTransactionReceipt} from "wagmi";
-import {wagmiFeedbackContract} from "@/config/wagmi";
-import {useToast} from "@/components/ui/use-toast";
-import {Chapter} from "@/types/chapter";
-import {useSapphire} from "@/hooks/useSapphireContractWrite";
+import { useEffect } from "react";
+import { Textarea } from "@/components/ui/textarea";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { Account } from "@/components/account";
+import { CircleUser, RefreshCw } from "lucide-react";
+import { StarRating } from "@/components/star-rating";
+import { useContractWrite, useWaitForTransactionReceipt } from "wagmi";
+import { wagmiFeedbackContract } from "@/config/wagmi";
+import { useToast } from "@/components/ui/use-toast";
+import { Chapter } from "@/types/chapter";
+import { useSapphire } from "@/hooks/useSapphireContractWrite";
 
 interface Props {
-    chapter: Chapter;
+  chapter: Chapter;
 }
 
-export default function FormGiveFeedback({chapter}: Props) {
-    const { toast } = useToast();
+export default function FormGiveFeedback({ chapter }: Props) {
+  const { toast } = useToast();
 
-    // const { data: hash, isPending, writeContract } = useContractWrite();
-    const { data: hash, isPending, writeContract } = useSapphire();
-    const { isLoading: isConfirming, isSuccess: isConfirmed } =
-        useWaitForTransactionReceipt({
-            hash,
-        })
+  // const { data: hash, isPending, writeContract } = useContractWrite();
+  const { data: hash, isPending, writeContract } = useSapphire();
+  const { isLoading: isConfirming, isSuccess: isConfirmed } =
+    useWaitForTransactionReceipt({
+      hash: hash,
+    });
 
-    useEffect(() => {
-        if (isConfirmed) {
-            toast({
-                title: "Success",
-                description: `Thank you for your feedback! Stay tuned for more updates.`,
-                status: "success",
-            });
-        }
-    }, [isConfirmed])
-
-    async function onSubmit(e) {
-        e.preventDefault();
-        const formData = new FormData(e.target);
-
-        const res = await writeContract({
-            ...wagmiFeedbackContract,
-            functionName: 'createFeedback',
-            args: [
-                BigInt(chapter.id),
-                formData.get('comment'),
-                formData.get('rating')
-            ],
-        })
+  useEffect(() => {
+    if (isConfirmed) {
+      toast({
+        title: "Success",
+        description: `Thank you for your feedback! Stay tuned for more updates.`,
+      });
     }
+  }, [isConfirmed, toast]);
 
-    if( isConfirmed ){
-        return null;
-    }
+  async function onSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
 
-    return (
-        <form onSubmit={onSubmit} className="flex flex-col gap-4 w-full">
-            <div className="flex gap-2 items-center">
-                <CircleUser className="h-5 w-5 flex-none"/>
-                <Account/>
-                <StarRating className="ml-auto" />
+    const res = await writeContract({
+      ...wagmiFeedbackContract,
+      functionName: "createFeedback",
+      args: [
+        BigInt(chapter.id),
+        formData.get("comment"),
+        formData.get("rating"),
+      ],
+    });
+  }
+
+  if (isConfirmed) {
+    return null;
+  }
+
+  return (
+    <form onSubmit={onSubmit} className="flex flex-col gap-4 w-full">
+      <div className="flex gap-2 items-center">
+        <CircleUser className="h-5 w-5 flex-none" />
+        <Account />
+        <StarRating className="ml-auto" />
+      </div>
+      <div className="min-w-0 flex-1">
+        <div className="relative w-full">
+          <div className="overflow-hidden rounded-lg shadow-sm border border-gray-300 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-primary">
+            <Label htmlFor="comment" className="sr-only">
+              Give your feedback
+            </Label>
+            <Textarea
+              rows={4}
+              name="comment"
+              id="comment"
+              className="block w-full resize-none border-0 bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 rounded-none ring-0 sm:text-sm font-mono sm:leading-6"
+              placeholder="Add your comment..."
+              defaultValue={""}
+            />
+
+            {/* Spacer element to match the height of the toolbar */}
+            <div className="py-2" aria-hidden="true">
+              {/* Matches height of button in toolbar (1px border + 36px content height) */}
+              <div className="py-px">
+                <div className="h-9" />
+              </div>
             </div>
-            <div className="min-w-0 flex-1">
-                <div className="relative w-full">
-                    <div className="overflow-hidden rounded-lg shadow-sm border border-gray-300 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-primary">
-                        <Label htmlFor="comment" className="sr-only">
-                            Give your feedback
-                        </Label>
-                        <Textarea
-                            rows={4}
-                            name="comment"
-                            id="comment"
-                            className="block w-full resize-none border-0 bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 rounded-none ring-0 sm:text-sm font-mono sm:leading-6"
-                            placeholder="Add your comment..."
-                            defaultValue={''}
-                        />
+          </div>
 
-                        {/* Spacer element to match the height of the toolbar */}
-                        <div className="py-2" aria-hidden="true">
-                            {/* Matches height of button in toolbar (1px border + 36px content height) */}
-                            <div className="py-px">
-                                <div className="h-9" />
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="absolute inset-x-0 bottom-0 flex justify-end py-2 pl-3 pr-2">
-                        <Button
-                            className="flex-shrink-0 min-w-[100px]"
-                            type="submit"
-                        >
-                            {isConfirming || isPending ? (
-                            <>
-                                <RefreshCw className="w-5 h-5 flex-none animate-spin" />
-                                { isPending && (<span>Waiting…</span>)}
-                                { isConfirming && (<span>Sending…</span>)}
-                            </>
-                            ) : (
-                                'Post'
-                            )}
-                        </Button>
-                    </div>
-                </div>
-            </div>
-        </form>
-    )
+          <div className="absolute inset-x-0 bottom-0 flex justify-end py-2 pl-3 pr-2">
+            <Button className="flex-shrink-0 min-w-[100px]" type="submit">
+              {isConfirming || isPending ? (
+                <>
+                  <RefreshCw className="w-5 h-5 flex-none animate-spin" />
+                  {isPending && <span>Waiting…</span>}
+                  {isConfirming && <span>Sending…</span>}
+                </>
+              ) : (
+                "Post"
+              )}
+            </Button>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
 }
