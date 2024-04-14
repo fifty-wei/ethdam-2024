fd653b0bf52f8cf8c31d286414ff6aa5baf1b531
akugone <dev@akumaw.com>
2024-04-14 09:38:53 +0200

    Fix href in page.tsx to use absolute URL

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -396,7 +396,7 @@ export default function Home() {
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
-                href="fiftywei.co"
+                href="https://www.fiftywei.co/"
                 target="_blank"
                 className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
               >
